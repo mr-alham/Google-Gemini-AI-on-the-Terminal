7e0b5d3c4b8e00d8a3a323d725a4f28c3daad61d
alham_manjaro <86831608+mr-alham@users.noreply.github.com>
2024-05-18 19:27:50 +0530

    finalized project

---
+++ colors.py
@@ -0,0 +1,65 @@
+#!/bin/env python3
+"""ansi color codes"""
+
+
+def ansi(colors: list[str]) -> str:
+    """Returns a string of ansi color codes"""
+
+    tags = {
+        "reset": "\033[0m",
+        "bold": "\033[1m",
+        "dim": "\033[2m",
+        "italic": "\033[3m",
+        "underline": "\033[4m",
+        "blink": "\033[5m",
+        "inverse": "\033[7m",
+        "hide": "\033[8m",
+        "strikethrough": "\033[9m",
+
+        "black": "\033[30m",
+        "red": "\033[31m",
+        "green": "\033[32m",
+        "yellow": "\033[33m",
+        "blue": "\033[34m",
+        "magenta": "\033[35m",
+        "cyan": "\033[36m",
+        "white": "\033[37m",
+
+        "bright_black": "\033[90m",
+        "bright_red": "\033[91m",
+        "bright_green": "\033[92m",
+        "bright_yellow": "\033[93m",
+        "bright_blue": "\033[94m",
+        "bright_magenta": "\033[95m",
+        "bright_cyan": "\033[96m",
+        "bright_white": "\033[97m",
+
+        "black_bg": "\033[40m",
+        "red_bg": "\033[41m",
+        "green_bg": "\033[42m",
+        "yellow_bg": "\033[43m",
+        "blue_bg": "\033[44m",
+        "magenta_bg": "\033[45m",
+        "cyan_bg": "\033[46m",
+        "white_bg": "\033[47m",
+
+        "black_bright_bg": "\033[100m",
+        "red_bright_bg": "\033[101m",
+        "green_bright_bg": "\033[102m",
+        "yellow_bright_bg": "\033[103m",
+        "blue_bright_bg": "\033[104m",
+        "magenta_bright_bg": "\033[105m",
+        "cyan_bright_bg": "\033[106m",
+        "white_bright_bg": "\033[107m",
+    }
+
+    string = tags["reset"]
+
+    for color in colors:
+        try:
+            string += tags[color]
+        except KeyError:
+            print("Invalid color:", color)
+            continue
+
+    return string
--- main.py
+++ main.py
@@ -2,24 +2,65 @@
 """This script is to chat with Google Gemini AI"""
 
 
-from json import JSONDecodeError, load
-from os import path
-from sys import argv, exit
+import json
+import os
+import sys
+import re
 
 import google.generativeai as genai
 import PIL.Image
-from pyperclip import paste
+import pyperclip as clipboard
+from colors import ansi
+
+
+def format_text(text: str) -> str:
+    patterns = {
+        r"```(?P<content>.*?)```": [ansi(["black_bg"]), ansi(["white"])],  # code
+        r"\*\*\*(?P<content>.*?)\*\*\*": [ansi(["bold", "italic"]), ansi(["white"])],  # bold_italic
+        r"\*\*(?P<content>.*?)\*\*": [ansi(["bold", "yellow"]), ansi(["white"])],  # bold
+        r"\*(?P<content>.*?)\*": [ansi(["italic", "cyan"]), ansi(["white"])],  # italic
+        r"\`(?P<content>.*?)\`": [ansi(["dim", "bright_green"]), ansi(["white"])],  # code
+        r"^\>.?(?P<content>.*)": [ansi(["bright_black", "white_bg"]), ansi(["white"])],  # blockquote
+        r"^#{1,6}(?P<content>.*)": [ansi(["dim", "bold", "underline", "magenta"]), ansi(["white"])],  # headings
+        # r"#{1,6}(?P<content>.*)": [ansi(["red"]), ansi(["reset", "white"])],  # headings
+        r"^\>(?P<content>.*?)": [ansi(["black"]), ansi(["white_bg", "reset"])],  # blockquote
+        r"^\* (?P<content>.*?)": ["\u2022", ""],  # un ordered lists
+        r"\~\~(?P<content>.*?)\~\~": [ansi(["strikethrough"]), ansi(["white"])],  # strikethrough
+    }
+
+    other_patterns = {
+        # r"\b(\d+)\b": [ansi(["yellow"]),ansi(["reset", "yellow","white"])],  # digits  *something is going wrong with the digits
+        r"(https?://\S+|www\.\S+)": [ansi(["blue", "underline", "italic"]), ansi(["reset", "white"])],  # url
+    }
+
+    for pattern in other_patterns:
+        matches = re.finditer(pattern, text, re.MULTILINE)
+
+        for match in matches:
+            formatted_text = other_patterns[pattern][0] + \
+                match.group(0) + other_patterns[pattern][1]
+            text = text.replace(match.group(0), formatted_text)
+
+    for pattern in patterns:
+        matches = re.finditer(pattern, text, re.MULTILINE)
+
+        for match in matches:
+            formatted_text = patterns[pattern][0] + \
+                match.group("content") + patterns[pattern][1]
+            text = text.replace(match.group(0), formatted_text)
+
+    return text
 
 
 def initialize_genai():
     """Initialize the generative AI module and configuring it."""
 
     try:
-        script_dir = path.dirname(path.realpath(__file__))
-        file = path.join(script_dir, "keys.json")
+        script_dir = os.path.dirname(os.path.realpath(__file__))
+        file = os.path.join(script_dir, "keys.json")
 
         with open(file, "r", encoding="utf-8") as f:
-            content = load(f)
+            content = json.load(f)
             gemini_api_key = content["GEMINI_API_KEY"]
 
             if gemini_api_key == "your gemini api key here":
@@ -28,7 +69,7 @@ def initialize_genai():
                 print("create a key in Google AI Studio.'https://aistudio.google.com/app/apikey'")
                 print("save the key to 'keys.json' file.")
 
-                exit(1)
+                sys.exit(1)
 
             gemini_model = content["GEMINI_MODEL"]
             safety_settings = content["SAFETY_SETTINGS"]
@@ -39,24 +80,22 @@ def initialize_genai():
     except FileNotFoundError:
         print("Could not find the configuration file 'keys.json', Ensure it exists")
 
-        exit(1)
+        sys.exit(1)
 
-    except JSONDecodeError:
-        print(
-            "Error: Could not parse the key file 'keys.json'. Please check the file format."
-        )
+    except json.JSONDecodeError:
+        print("Error: Could not parse the key file 'keys.json'. Please check the file format.")
 
-        exit(1)
+        sys.exit(1)
 
     except KeyError as e:
         print(f"Error: Missing key '{e.args[0]}' in 'keys.json' file.")
 
-        exit(1)
+        sys.exit(1)
 
     except Exception as e:
         print("an exception occurred as: ", e)
 
-        exit(1)
+        sys.exit(1)
 
     model = genai.GenerativeModel(
         model_name=gemini_model,
@@ -67,27 +106,76 @@ def initialize_genai():
     return model
 
 
-def response_with_images():
-    """when need a response with a image"""
+def text_to_text():
+    """Input: text  ->  output: text"""
+
+    print(ansi(["dim", "bright_yellow", "bold"]),
+          "You are using Text-to-Text(T2T) Model.\n Enter 'Image Mode' to turn to Image/text Model", ansi([]))
+
+    model = initialize_genai()
+
+    chat = model.start_chat(history=[])
 
     while True:
-        image_file = input("The path to the image ('clip' to copy from the clipboard or press 'enter' key): ")
+        query = input(ansi(["green", "bold"])+"Ask Me: "+ansi(["white"]))
 
-        if image_file == "clip" or image_file == "":
-            image_file = paste()
+        if query == "" or query.isspace():
+            print(ansi(["dim", "red"]) + "The query is empty.\n")
+            continue
 
-        if path.exists(image_file):
-            query = input("Query: ")
+        if query == "Image Mode":
+            print(ansi([]))
+            response_with_images()
+
+        try:
+            response = chat.send_message(query, stream=False)
+            print(ansi(["green", "bold"]) + "Response: ", ansi(["white"]))
+            print(format_text(response.text)+ansi([]))
+
+        except ValueError:
+            print(response.prompt_feedback)
+            print(response.candidates[0].finish_reason)
+            print(response.candidates[0].safety_ratings)
+            print()
+
+        print(ansi(["dim", "bold", "blue"])+"_ " *
+              int(os.get_terminal_size().columns/2))
+
+
+def response_with_images():
+    """Input: text/image  ->  output: text"""
+
+    print(ansi(["dim", "bright_yellow", "bold"]),
+          "You are using MultiModel Model.\n WEnter 'Text Mode' to turn to Text-to-Text Model", ansi([]))
+
+    while True:
+        print(ansi(["green", "bold"]) + "The path to the image " + ansi(["green", "bold", "dim"]) +
+              "(Enter 'clip' or press 'enter' key to copy from the Clipboard): " + ansi(["white"]), end='')
+        image_file = input()
+
+        if image_file == "Text Mode":
+            print(ansi([]))
+            text_to_text()
+
+        if image_file in ("clip", ""):
+            image_file = clipboard.paste()
+
+        if os.path.exists(image_file):
+            print(ansi(["green", "bold"]) +
+                  "Prompt: " + ansi(["white"]), end="")
+            query = input()
             model = initialize_genai()
 
             response = model.generate_content(
                 [query, PIL.Image.open(image_file)], stream=False
             )
 
-            print(response.text)
+            print(format_text(response.text), ansi([]))
 
         else:
-            print(f"Error: Couldn't find the image '{image_file}'.")
+            print(f"{ansi(["bold", "red"])}Error: Not a valid file path: {
+                  ansi(["dim", "bright_red"])}'{image_file}'."
+            )
             print("Try again.\n")
 
             continue
@@ -96,42 +184,46 @@ def response_with_images():
 def main():
     """main logic of the script is happening here"""
 
-    if len(argv) > 1:
-        if "--image" in argv:
+    os.system("clear -x")
+
+    size = os.get_terminal_size()
+    phrase = "Gemini-AI on Terminal by ALHAM"
+    space = (int(size.columns) - len(phrase))/2
+
+    # print(ansi(["dim", "yellow", "bold"]), "-" * (size.columns - 3), ansi([]))
+    print(" " * int(space) + ansi(["bright_cyan", "bold"]) + phrase + ansi([]))
+
+    if len(sys.argv) > 1:
+        if "--image" in sys.argv:
             response_with_images()
         else:
-            print(f"Error: Unknown argument: {argv[1]}")
+            print(f"Error: Unknown argument: {sys.argv[1]}")
 
-            exit(1)
+            sys.exit(1)
 
-    model = initialize_genai()
-
-    chat = model.start_chat(history=[])
-
-    while True:
-        query = input("what do you need to ask: ")
-
-        try:
-            response = chat.send_message(query, stream=False)
-            print(response.text)
-
-        except genai.RequestError as e:
-            print(
-                f"Error: An error occurred while communicating with the Gemini API. ({e})"
-            )
-
-        except ValueError:
-            print(response.prompt_feedback)
-            print(response.candidates[0].finish_reason)
-            print(response.candidates[0].safety_ratings)
-            print()
-
-        print("***" * 60)
-        print("\n")
-
-    # for chunk in response:
-    # print(chunk.text)
+    text_to_text()
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+
+        text = ansi(["bold", "bright_red"]) + "___ See ya! ___" + ansi(["bold","bright_white"])
+
+        print(f"{text:^{os.get_terminal_size().columns - 6}}")
+
+        space = int((os.get_terminal_size().columns/2) - 12)
+
+        print(" " * space + r"     .--.  ")
+        print(" " * space + r"    |o_o |  ")
+        print(" " * space + r"    |:_/ |  ")
+        print(" " * space + r"   //   \ \  ")
+        print(" " * space + r"  (|     | )  ")
+        print(" " * space + r" /'\_   _/`\  ")
+        print(" " * space + r" \___)=(___/  ")
+
+        print(ansi([]))
+
+        sys.exit(0)
+
